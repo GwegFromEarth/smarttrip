1c0ace12d1f1edcf2072e8dbd7219523e9cc812d
Grégory LISSAC <greg.lissac@gmail.com>
2026-08-14 11:13:41 +0200

    feat: initialize Spring Boot backend

---
+++ backend/smarttrip-api/src/main/java/com/smarttrip/smarttrip_api/SmarttripApiApplication.java
@@ -0,0 +1,13 @@
+package com.smarttrip.smarttrip_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarttripApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarttripApiApplication.class, args);
+	}
+
+}
---
+++ backend/smarttrip-api/src/test/java/com/smarttrip/smarttrip_api/SmarttripApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smarttrip.smarttrip_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarttripApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
